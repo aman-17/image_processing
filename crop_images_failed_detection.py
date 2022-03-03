8a08d9b3a9209697c51fe3c8501d471d0ac4561b
aman-17 <aman.18bcn7075@vitap.ac.in>
2022-03-03 14:01:02 +0530

    crop images_failed

---
+++ crop_images_failed_detection.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('IMG_1565.jpg')
+w=(img.shape)
+width=int(w[1]/2)
+height=int(w[0]/2)
+
+print(width,height)
+cv2.imshow("original", img)
+
+# Cropping an image
+
+cropped_image = img[0:width, 0:height]
+
+# Display cropped image
+cv2.imshow("cropped", cropped_image)
+
+# Save the cropped image
+cv2.imwrite("Cropped_Image.jpg", cropped_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
